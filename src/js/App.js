94fb7f602a9f08bc1786897fa1cf75228c7ac033
Jim YuL <jimyu0414@gmail.com>
2018-08-27 16:45:24 +1000

    setup svg js file, add body background for index page

--- src/js/App.js
+++ src/js/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import logo from '../logo.svg';
+// import logo from '../logo.svg';
+import SVG from './SVG';
 import '../App.css';
 
 class App extends Component {
@@ -7,7 +8,8 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+
+          <SVG name="react-logo" className="App-logo" fillColor="#fff"/>
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
