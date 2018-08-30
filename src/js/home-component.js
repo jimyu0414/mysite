397e7154f1b73fb9ad67ce9feef4d8796363d909
Jim YuL <jimyu0414@gmail.com>
2018-08-30 17:07:46 +1000

    add svgs and images

--- src/js/home-component.js
+++ src/js/home-component.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react';
 import SVG from './svg';
+import sheepGif from '../img/smoke-sheep.gif';
 // import TextLoop from 'react-text-loop';
 
 class Home extends Component {
   state = {
-    professions: ['GRAPHIC DESIGNER','WEB DESIGNER','FRONT-END DEVELOPER','WEB DEVELOPER'],
+    professions: ['WEB DESIGNER','FRONT-END DEVELOPER','GUITAR PLAYER','SNOWBOARDER','INTERPRETER'],
     profession: 'DIGITAL DESIGNER',
     proCount: 0,
   }
@@ -31,8 +32,22 @@ class Home extends Component {
     return(
       <div className="front-page">
         <div className="container">
-          <h1>YU LIU</h1>
-          <h2>{this.state.profession}</h2>
+          <div className="front-page__content">
+            <h1>YU LIU</h1>
+            <h2>{this.state.profession}</h2>
+            <div className="divider"></div>
+            <p>
+              Hi there, my name is Yu Liu. I’ve been working in Web Industry for 3 years. Designing websites and all assets. I also take part in front-end web development and specialised in HTML, CSS/SASS, JS, NPM, Webpack, GIt, etc. Before I became a web developer I work as a freelance graphic designer, working with logos, flyers, posters, websites, etc.
+            </p>
+          </div>
+          <div className="front-page__image">
+            <div className="front-page__image--professions">
+
+            </div>
+            <div className="front-page__image--sheep">
+                <img src={sheepGif} alt='sheep' />
+            </div>
+          </div>
         </div>
       </div>
     );
